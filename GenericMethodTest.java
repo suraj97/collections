2e34bfe82f83d67a70106d01dc064b4f0d6bdb45
suraj97 <55835075+suraj97@users.noreply.github.com>
2019-10-10 18:59:25 +0530

    Add files via upload

---
+++ GenericMethodTest.java
@@ -0,0 +1,28 @@
+package com.quinnox.connections;
+
+public class GenericMethodTest {
+	
+	public static <E> void printArray(E[] inputArray)
+	{
+		for(E element : inputArray)
+		{
+			System.out.printf("%s",element);
+		}
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer[] intArray = {1,2,3,4,5};
+		Double[] doubleArray = {1.1,2.2,3.3,4.4};
+		Character[] charArray = {'H','E','L','L','O'};
+		
+		System.out.println("Array integerArray contains");
+		printArray(intArray);
+		
+		System.out.println("Array doubleArray contains");
+		printArray(doubleArray);
+		
+		System.out.println("Array characterArray contains");
+		printArray(charArray);
+	}
+}
